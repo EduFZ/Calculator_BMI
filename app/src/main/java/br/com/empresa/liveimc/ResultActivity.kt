16d94b3e5cc2ead4b77e9d549d9565b15a9f2af1
Eduardo F Zimmermann <edu.zimmermann.efz@gmail.com>
2022-08-19 19:29:54 -0300

    Estilizado EditText e novo LinearLayout

---
+++ app/src/main/java/br/com/empresa/liveimc/ResultActivity.kt
@@ -0,0 +1,12 @@
+package br.com.empresa.liveimc
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.liveimc.R
+
+class ResultActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_result)
+    }
+}
